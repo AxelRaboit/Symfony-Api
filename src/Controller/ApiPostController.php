73ee483e819396111c9a2c23f0ba0ecbc44e18d0
Axel Raboit <axel.raboit@gmail.com>
2022-01-22 14:56:06 +0100

    refactoring the code about the index methodfor the api

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -4,26 +4,21 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiPostController extends AbstractController
 {
     /**
      * @Route("/api/post", name="api_post", methods={"GET"})
      */
-    public function index(PostRepository $postRepository, NormalizerInterface $normalizer): Response
+    public function index(PostRepository $postRepository, SerializerInterface $serializerInterface): Response
     {
         $posts = $postRepository->findAll();
 
-        $postsNormalized = $normalizer->normalize($posts, null, ['groups' => 'post:read']);
-        
-        $json = json_encode($postsNormalized);
-
-        $response = new Response($json, 200, [
-            "Content-Type" => "application/json"
-        ]);
+        $response = $this->json($posts, 200, [], ['groups' => 'post:read']);
 
         return $response;
 
